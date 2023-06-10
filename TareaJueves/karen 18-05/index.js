ab4aa30c52fced9a4de8f95d980aa7dfbed1598e
Jeremias-Stuer <jeremiasstuer@gmail.com>
2023-06-09 22:46:34 -0300

    tarea

--- TareaJueves/karen 18-05/index.js
+++ TareaJueves/karen 18-05/index.js
@@ -1,18 +1,22 @@
  async function hacerRequest() {
    try {
-    var url = ' https://pokeapi.co/api/v2/pokemon/ditto'; 
+    var url = ' https://digimon-api.vercel.app/api/digimon'; 
 
     var response = await fetch(url);
-     var data = await response.json();
+    var data = await response.json();
+    for(let dat of data){
+      document.getElementById("contenedor").innerHTML+=`
+      <div class="cont-datos">
+        <div class="cont-img">
+          <img src="${dat.img}" alt="">
+        </div>
+        <div class="cont-names">
+        <h1>${dat.name}</h1>
+        </div> 
+      </div>
+      `
+    }
 
-     document.getElementById("contenedor").innerHTML=`
-     <h1>${data.name}</h1>
-     <p>Habilidades</p>
-     <ul>
-        <li>${data.abilities[0].ability.name}</li>
-        <li>${data.abilities[1].ability.name}</li>
-    </ul>
-     `
     console.log(data);
    } catch (error) {
      console.log(error);
@@ -20,3 +24,4 @@
  }
 
  hacerRequest();
+
